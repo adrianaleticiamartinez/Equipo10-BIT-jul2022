a6f3205cabbc0cf6e29dc7c5debd0e929d128838
Sharonit <99165918+Sharonit@users.noreply.github.com>
2022-07-14 11:33:29 -0500

    Update buscar.py

--- buscar.py
+++ buscar.py
@@ -1,11 +1,38 @@
 import csv
 
+#print("Ingrese el identificador del usuario")
+#uid= input()
+#print("Ingrese el password")
+#password = input()
+
+uid = 'EduardoE'
 with open('baseUsuarios.csv') as file:
     reader = csv.reader(file)
     for row in reader:
         datos=row
-        perfil = datos[6]
-     #   print(perfil)
-        if(perfil == "Manager"):
+        if(datos[0] == uid):
+            usuario = datos[0]
+            perfil = datos[6]
             print(datos)
-    
+
+print("Ingrese el iID del Cliente")
+idCliente = input()
+
+def buscarUsuarios(idCliente,perfil):
+    if(perfil == "Manager"):
+        print('entrando a Manager')
+        with open('baseClientesHackaton2022.csv') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                datos = row
+                if(datos[0] == idCliente):
+                    print(datos)
+                    break
+
+    elif(perfil == "Validador"):
+        print()
+
+    elif(perfil == "Restringido"):
+        print()
+
+buscarUsuarios(idCliente,perfil)
