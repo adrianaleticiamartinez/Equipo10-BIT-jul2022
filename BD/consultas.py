fd77c74da1aeda3201e8c19b7fcd42a2a47ef8c7
JonasJP13 <109187932+JonasJP13@users.noreply.github.com>
2022-07-14 11:23:44 -0500

    libro

--- BD/consultas.py
+++ BD/consultas.py
@@ -3,9 +3,10 @@ import mysql.connector
 db=mysql.connector.connect(host="localhost", user="root", passwd="",database="bbva")
 conecta=db.cursor()
 
-dato = input("Dato: ")
 
-query = "SELECT idCliente,nombre,nacionalidad FROM libropeque WHERE nombre = 'Abril'"
+
+dato = input("idCliente: ")
+query = "SELECT idCliente,nombre,nacionalidad FROM libropeque WHERE idCliente = '{}'".format(dato)
 conecta.execute(query)
 
 usuarios = conecta.fetchall()
@@ -13,8 +14,4 @@ for x in usuarios:
     print (x)
 
 db.close()
-
-
-
-
 
