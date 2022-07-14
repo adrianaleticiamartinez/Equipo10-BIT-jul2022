f1d338c195da72dc1be8882b151096e3d4acda24
Angel-Altamirano <altamirano.ipn@gmail.com>
2022-07-14 11:04:22 -0500

    Archivos para carga de BD
    Archivos para carga de BD

--- BD/Carga.py
+++ BD/Carga.py
@@ -1,4 +1,15 @@
 import mysql.connector
+import csv
+
+count=0
+with open(r"C:\Users\Angel\Desktop\BBVA BIT\Python\Equipo10-BIT-jul2022\BD\baseClientesHackaton2022.csv", newline='') as File:  
+    reader = csv.reader(File, delimiter=',')
+    for row in reader:
+        print(row)
+        count = count + 1
+        if(count == 10):
+            exit()
+
 
 conexion1=mysql.connector.connect(host="localhost", user="root", passwd="",database="bbva")
 conecta=conexion1.cursor()
@@ -6,4 +17,5 @@ conecta.execute("show tables")
 for base in conecta:
     print(base)
 
+conecta.close()
 conexion1.close()  
