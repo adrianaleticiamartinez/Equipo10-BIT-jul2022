1e48535470ca2252d87193a8cc28b22aaaeed525
JonasJP13 <109187932+JonasJP13@users.noreply.github.com>
2022-07-14 11:47:28 -0500

    Prueba con perfiles

--- BD/consultas.py
+++ BD/consultas.py
@@ -1,14 +1,31 @@
+from ast import If
 import mysql.connector
 
 db=mysql.connector.connect(host="localhost", user="root", passwd="",database="bbva")
 conecta=db.cursor()
 
-
-
-dato = input("idCliente: ")
-query = "SELECT idCliente,nombre,nacionalidad FROM libropeque WHERE idCliente = '{}'".format(dato)
+#Consulta para manager
+dato = input("idCliente para manager: ")
+query = "SELECT * FROM libropeque WHERE idCliente = '{}'".format(dato)
 conecta.execute(query)
+usuarios = conecta.fetchall()
+for x in usuarios:
+    print (x)
 
+
+#Consulta para restringuida
+dato1 = input("idCliente restringuido: ")
+query = "SELECT nombre,apellidoPaterno,sexo,rfc FROM libropeque WHERE idCliente = '{}'".format(dato1)
+conecta.execute(query)
+usuarios = conecta.fetchall()
+for x in usuarios:
+    print (x)
+
+#Consulta para validador
+dato3 = input("idCliente: ")
+
+query = "SELECT nombre FROM libropeque WHERE idCliente = '{}'".format(dato3)
+conecta.execute(query)
 usuarios = conecta.fetchall()
 for x in usuarios:
     print (x)
